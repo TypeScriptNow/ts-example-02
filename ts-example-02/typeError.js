fb658066014c2398a779a4feea23e8bc6f648d55
Sobieck00 <thomas@sobieck.com>
2017-02-11 08:43:26 -0600

    First bits of this type error.

---
+++ ts-example-02/typeError.js
@@ -0,0 +1,36 @@
+﻿
+function printAllName(names, el) {
+
+    for (var i = 0; i < names.length; i++) {
+
+        el.innerHTML += names[i] + " ";
+    }
+}
+
+
+function loopOverNamesOne() {
+
+    var el = document.getElementById("namesOne");
+
+    var names = ["Guy", "Gidget", "Zoe", "Blue", "Lisa", "Thomas"];
+
+    printAllName(names, el);
+
+}
+
+function loopOverNamesTwo() {
+    var el = document.getElementById("namesTwo");
+
+    var names = '"Guy", "Gidget", "Zoe", "Blue", "Lisa", "Thomas"';
+
+    printAllName(names, el);
+}
+
+
+window.onload = function () {
+
+    loopOverNamesOne();
+
+    loopOverNamesTwo();
+    
+}
